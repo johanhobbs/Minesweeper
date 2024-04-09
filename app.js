bbbd0f4b9ceb9ffb8306a404e2878f4d37644446
johanhobbs <139718130+johanhobbs@users.noreply.github.com>
2024-04-09 14:57:08 +1200

    readline example

--- app.js
+++ app.js
@@ -1,67 +1,25 @@
-/*
-Create a function that takes a grid of $ and - where each ($) represents a mine and each (-) represents a safe spot.
-Return an array where each (-) is replaced by a digit indicating the number of immediately adjacent mines.
-
-mineGrid([
-    ["-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-"],
-    ["-", "-", "$", "-", "-"],
-    ["-", "-", "-", "-", "-"],
-    ["-", "-", "-", "-", "-"]
-])
-
-Output ->[
-    ["0", "0", "0", "0", "0"],
-    ["0", "1", "1", "1", "0"],
-    ["0", "1", "$", "1", "0"],
-    ["0", "1", "1", "1", "0"],
-    ["0", "0", "0", "0", "0"]
-]
-*/
-
-// let grid = [];
-// const gridHeight = 5;
-// const gridWidth = 5;
-
-// const createGrid = (gridHeight, gridWidth) => {
-//   for (i = 0; i < gridHeight; i++) {
-//     for (j = 0; j < gridWidth; j++) {
-//       i == 2 && j == 2 ? grid[i][j].push("$") : grid[i][j].push("-");
-//     }
-//   }
-//   return grid;
-// };
-
-// console.log(createGrid(gridHeight, gridWidth));
-
-// NOTE create a grid that accepts a 2D array
-const mineGrid = (arr) => {
-  // NOTE Create a helper function that acess the element position (y,x) in arr. If the Y index is valid or exists
-  // I want to return arr[y][x]; otherwise return null (this will prevent accessing undefined elements and avoid errors)
-  const checkAccess = (y, x) => (arr[y] ? arr[y][x] : null);
-  // NOTE Create a helper function that calculates the number of adjacent mines around the cell at position (y,x)
-  const getMine = (y, x) => {
-    // NOTE an array is constructed with elements representing the content of adjacent cells,
-    // We are going to use checkAccess for each of the eight possible adjacent positions
-    return [
-      checkAccess(y + 1, x), // checks the cell directly below the current cell (y,x)
-      checkAccess(y + 1, x - 1), // checks the cell diagonally below and to the left of the current cell
-      checkAccess(y + 1, x + 1), // checks the cell diagonally below and to the right of the current cell
-      checkAccess(y, x + 1), // checks the cell directly to the right of the current cell
-      checkAccess(y, x - 1), // checks the cell directly to the left of the current cell
-      checkAccess(y - 1, x), // checks the cell directly above the current cell
-      checkAccess(y - 1, x + 1), // checks the cell diagonally above and to the right of the current cell
-      checkAccess(y - 1, x - 1), // checks the cell diagonally above and to the left of the current cell
-      // NOTE Constuct an array of values representing the contents of the cells adjacent,
-      // after the contructing of the array, we will use filter method
-    ]
-      .filter((adjCell) => adjCell === "$")
-      .length.toString();
-  };
-  return arr.map((r, y) => r.map((c, x) => (c === "-" ? getMine(x, y) : c)));
-};
+// NOTE
 
 /*
-
-
+readline is a JS object (rl)
 */
+
+// Step 1: include the readline module
+
+const readline = require("readline");
+
+// Step 2: Create an instance of readline.interface
+
+const rl = readline.Interface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// Step 3: prompt the user for their name
+rl.question("What is your name?", (name) => {
+  // Step 4: Display greeting
+  console.log(`Hello, ${name}!`);
+});
+
+//Step 5: close the readline interface
+rl.close;
